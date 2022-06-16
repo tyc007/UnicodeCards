817b449a1bc4b806ebb66a406533ac1001339b88
tyc007 <tonychan550@gmail.com>
2022-06-15 17:23:12 -0700

    Created deck class file

---
+++ js/deck.js
@@ -0,0 +1,16 @@
+const SUITS = ['♠','♥','♣','♦'];
+const VALUE = [ "A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+class Deck{
+    constructor(cards) {
+        this.cards = cards;
+    }
+}
+
+class Card {
+    constructor(suit, value) {
+        this.suit = suit;
+        this.value = value;
+    }
+}
+
