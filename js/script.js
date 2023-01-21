8b2b5257ba22f8a53d5b7a3fef346d384c9590b2
tyc007 <tonychan550@gmail.com>
2023-01-20 19:00:00 -0800

    hit logic

--- js/script.js
+++ js/script.js
@@ -1,7 +1,45 @@
 import Deck from "./deck.js";
 import CardCodePoints from "../json/StandardDeck.json" assert { type: "json" };
 
-const playerHand = document.getElementById("player-hand")
+//TODO: 
+//Deal Cards
+//Add Options for Hit Stay
+//Scoring 
+
+const playerHand    = document.getElementById("player-hand");
+const dealButton    = document.getElementById("deal");
+const hitButton     = document.getElementById("hit")
+
+let deck;
+
+window.onload = function() {
+    buildDeck();
+    startGame();
+}
+
+function startGame() {
+    console.log(deck.cards);
+    for (let i = 0; i < 2; i+=1) {
+        let card = deck.cards.pop();
+        playerHand.insertAdjacentHTML('afterend', getCardUnicode(card));
+    }
+
+    hitButton.addEventListener("click", hit);
+
+}
+
+function buildDeck() {
+    deck = new Deck();
+    deck.shuffle();   
+}
+
+function hit(){
+    let card = deck.cards.pop();
+    playerHand.insertAdjacentHTML('afterend', getCardUnicode(card));
+}
+
+
+//dealBtn.addEventListener("click", function () { deal(deck)});
 
 function unicodeFromCodePoint(num){
     return String.fromCodePoint(parseInt(num,16));
@@ -11,37 +49,13 @@ function getCardDetails(card) {
     return CardCodePoints.filter(CardCodePoints => CardCodePoints.rank == card.rank && CardCodePoints.suit == card.suit)[0];
 }
 
-function getCardByCodePoint(codepoint) {
-    return CardCodePoints.filter(CardCodePoints => CardCodePoints.codepoint == codepoint);
+function getCardUnicode(card) {
+    let cardDetails = getCardDetails(card);
+    let cardUnicode = unicodeFromCodePoint(cardDetails.codepoint);
+    return cardUnicode;
 }
 
-document.addEventListener('click', () => {
-    
-})
-
-startGame()
-function startGame() {
-    const deck = new Deck();
-    deck.shuffle()
-    
-    console.log(deck.cards);
-
-    for (let i = 0; i < 2; i+=1) {
-        let card = deck.cards.pop()
-        console.log(card);
-        let cardDetails = getCardDetails(card)
-        let cardUnicode = unicodeFromCodePoint(cardDetails.codepoint)
-        playerHand.insertAdjacentHTML('afterend', cardUnicode);
-    }
-    
-
-    //document.getElementById('player-hand').innerHTML = 
-    //TODO: 
-    //Deal Cards
-    //Add Options for Hit Stay
-    //Scoring 
 
 
-}
 
 
