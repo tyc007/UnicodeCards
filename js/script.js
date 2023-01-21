40594d228fd2a98a6e943eee18be324d5db23d7a
tyc007 <tonychan550@gmail.com>
2023-01-20 18:30:19 -0800

    find unicode logic and display on page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 import Deck from "./deck.js";
-import data from "../json/StandardDeck.json" assert { type: "json" };
+import CardCodePoints from "../json/StandardDeck.json" assert { type: "json" };
 
-const playerHand = document.querySelector(".player-hand")
+const playerHand = document.getElementById("player-hand")
+
+function unicodeFromCodePoint(num){
+    return String.fromCodePoint(parseInt(num,16));
+}
+
+function getCardDetails(card) {
+    return CardCodePoints.filter(CardCodePoints => CardCodePoints.rank == card.rank && CardCodePoints.suit == card.suit)[0];
+}
+
+function getCardByCodePoint(codepoint) {
+    return CardCodePoints.filter(CardCodePoints => CardCodePoints.codepoint == codepoint);
+}
 
 document.addEventListener('click', () => {
     
@@ -13,14 +25,23 @@ function startGame() {
     deck.shuffle()
     
     console.log(deck.cards);
-    console.log(deck.cards.pop());
-    //document.getElementById('player-hand').innerHTML = String.fromCodePoint( parseInt(cardHex,16));
+
+    for (let i = 0; i < 2; i+=1) {
+        let card = deck.cards.pop()
+        console.log(card);
+        let cardDetails = getCardDetails(card)
+        let cardUnicode = unicodeFromCodePoint(cardDetails.codepoint)
+        playerHand.insertAdjacentHTML('afterend', cardUnicode);
+    }
+    
+
+    //document.getElementById('player-hand').innerHTML = 
     //TODO: 
     //Deal Cards
     //Add Options for Hit Stay
     //Scoring 
 
-    console.log(data);
+
 }
 
 
