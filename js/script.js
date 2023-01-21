e20a90b2eb621c1b99668eaff03a72564786a886
tyc007 <tonychan550@gmail.com>
2023-01-20 17:34:04 -0800

    import json for standard deck

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-import Deck from "./deck.js"
+import Deck from "./deck.js";
+import data from "../json/StandardDeck.json" assert { type: "json" };
 
-const deck = new Deck();
-deck.shuffle()
-console.log(deck.cards);
+const playerHand = document.querySelector(".player-hand")
+
+document.addEventListener('click', () => {
+    
+})
+
+startGame()
+function startGame() {
+    const deck = new Deck();
+    deck.shuffle()
+    
+    console.log(deck.cards);
+    console.log(deck.cards.pop());
+    //document.getElementById('player-hand').innerHTML = String.fromCodePoint( parseInt(cardHex,16));
+    //TODO: 
+    //Deal Cards
+    //Add Options for Hit Stay
+    //Scoring 
+
+    console.log(data);
+}
+
+
